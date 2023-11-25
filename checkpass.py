0434752f0d8d6bbbea8d56d64c081f15ea01f8fa
Kdocia <134646850+Kdocia@users.noreply.github.com>
2023-11-25 01:52:48 -0300

    Update checkpass.py

--- checkpass.py
+++ checkpass.py
@@ -11,11 +11,13 @@ with open('criptoPass.txt', 'r') as datahash:
 #Pegar a hash em hexadecinaml da senha informada no txt
 hash_senha = sha256(senha.encode()).hexdigest()
 
+
 #Comparar a senha fornecida pelo usuário com a senha armazenada:
 '''if hash_senha == cripto_senha:
     print(f'A senha {senha} está correta ')
 else:
     print(f'ERRO: A senha {senha} está incorreta')'''
+
 
 #teste realizado pelo pytest
 def test_answer():
